14405fd37e7223205350482cbd4f18ef8fd550f0
juancgv97 <juguzman18@poligran.edu.co>
2020-04-21 13:25:13 -0500

    Cambio icono de la pagina y cambios al modulo "films"

--- src/app/components/film/film.component.ts
+++ src/app/components/film/film.component.ts
@@ -3,17 +3,24 @@ import { FilmService } from 'src/app/services/film.service';
 import { Film } from 'src/app/models/film';
 
 
+
 @Component({
   selector: 'app-film',
   templateUrl: './film.component.html',
   styleUrls: ['./film.component.css']
+  
 })
 export class FilmComponent implements OnInit {
   public filmModel: Film = new Film();
+  
 
   constructor(
     private filmService: FilmService
-  ) { }
+   
+  ) {
+    
+
+   }
 
   ngOnInit() {
     this.getFilms();
